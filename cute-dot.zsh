c8d78f8fc706e7d6cb850db5e8921b17a5670459
fourdim <59462000+fourdim@users.noreply.github.com>
2023-06-22 22:08:08 +0800

    update

--- cute-dot.zsh
+++ cute-dot.zsh
@@ -21,30 +21,35 @@ _add-pf() {  # <pf-name> {<pf-loc> <pf-pat>}...
 alias -s pf='_add-pf'
 
 _rsync-pat() {  # <src> <dst> <pat>
-    cd $1 &>/dev/null &&
-    rsync $rsync_opt -R $~=3 $2/
+    cd $1 && rsync $rsync_opt --relative $~=3 $2/
 }
 
 _sync() {  # <pf-name>
     for i in $=pf_map[$1]; {
-        echo $CYAN"$1 <- ${(D)pf_loc[i]}"$NC
-        _rsync-pat $pf_loc[i] $DOT_DIR/$1 $pf_pat[i]
-        echo
+        local changes=$(_rsync-pat $pf_loc[i] $DOT_DIR/$1 $pf_pat[i])
+        if [[ $changes != '' ]] {
+            echo $CYAN"$1 <- ${(D)pf_loc[i]}"$NC
+            echo $changes
+            echo
+        }
     }
 }
 
 _apply() {  # <pf-name>
     for i in $=pf_map[$1]; {
-        echo $CYAN"$1 -> ${(D)pf_loc[i]}"$NC
-        _rsync-pat $DOT_DIR/$1 $pf_loc[i] $pf_pat[i]
-        echo
+        local changes=$(_rsync-pat $DOT_DIR/$1 $pf_loc[i] $pf_pat[i])
+        if [[ $changes != '' ]] {
+            echo $CYAN"$1 -> ${(D)pf_loc[i]}"$NC
+            echo $changes
+            echo
+        }
     }
 }
 
 source $(which env_parallel.zsh)
 
 _init() {
-    setopt null_glob extended_glob no_bare_glob_qual
+    setopt null_glob extended_glob
 }
 
 _for-each-pf() {  # <func> [--all | <pf-name>...]
@@ -65,12 +70,13 @@ cute-dot-apply() { _for-each-pf _apply $@ }
 rsync_opt=(
     '--recursive'
     '--mkpath'
+    '--checksum'
     '--itemize-changes'
 )
 
 zsh.pf \
     ~ '.zshenv' \
-    ~/.config/zsh '.zshrc *.zsh (^.*)/(^*.zwc)'
+    ~/.config/zsh '.zshrc *.zsh */^*.zwc'
 
 # systemd.pf \
 #     ~/.config 'user-tmpfiles.d/*'
@@ -102,8 +108,8 @@ pip.pf \
 # direnv.pf \
 #     ~/.config/direnv '*'
 
-fontconfig.pf \
-    ~/.config/fontconfig '*'
+# fontconfig.pf \
+#     ~/.config/fontconfig '*'
 
 # paru.pf \
 #     ~/.config/paru '*'
